c41995f5682b5716bf12622a656fb9a6d4aa9fa2
rohitjoshi6 <rohitjoshi632001@gmail.com>
2021-01-31 13:08:18 +0530

    Added click me button

--- demo/peer1.js
+++ demo/peer1.js
@@ -3,7 +3,9 @@ const localConnection = new RTCPeerConnection()
 
 localConnection.onicecandidate = e =>  {
     console.log(" NEW ice candidnat!! on localconnection reprinting SDP ")
-    $('#offer').text(JSON.stringify(localConnection.localDescription))
+    $("#create-offer").click(()=>{
+        alert(JSON.stringify(localConnection.localDescription))
+    })
 }
 
 
@@ -12,30 +14,7 @@ const sendChannel = localConnection.createDataChannel("sendChannel");
    sendChannel.onopen = e => $('#status').text('Open')
      sendChannel.onclose =e => $('#status').text('Close')
 
-function myFunction() {
-        document.getElementById("offer").innerHTML = localConnection.createOffer().then(o => localConnection.setLocalDescription(o)) ;
-      }
-
-$('#offer').click(()=>{
-    fetch(localConnection.createOffer().then(o => localConnection.setLocalDescription(o)))
-    .then(response=>response.json())
-    .then(data=>{
-        console.log()
-    })
-    //var msg;
-    //msg=localConnection.createOffer().then(o => localConnection.setLocalDescription(o));
-    //alert(msg);
-})
-
-//localConnection.createOffer().then(o => localConnection.setLocalDescription(o))
-
-/*$('button.msg').on('click', function(e) {
-    var msg=JSON.stringify(localConnection.createOffer().then(o => localConnection.setLocalDescription(o)));
-    
-    alert(offer+"\n"+msg)
-  });*/
-
-
+localConnection.createOffer().then(o => localConnection.setLocalDescription(o))
 
 $('#connect').click(() => {
     let answer = $('#answer').val()
