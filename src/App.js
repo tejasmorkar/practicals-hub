8ac8fa4569e1d9366749b35cd22bec98c489bcbe
tejasmorkar <tejasmorkar@gmail.com>
2021-04-14 21:12:39 +0530

    Add wireframes, assets, and web.config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from "./logo.svg";
+import React from "react";
+import { ReactComponent as Logo } from "./assets/logo.svg";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Practicals Hub</h1>
+                <Logo height="54px" width="54px" />
+                <span style={{ fontSize: "1.5em", margin: "0 1em" }}>
+                    Practicals Hub
+                </span>
             </header>
         </div>
     );
