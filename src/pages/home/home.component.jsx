5a50653ab4c7c65ee6402b518eca64642941fe94
tejasmorkar <tejasmorkar@gmail.com>
2021-04-16 11:31:04 +0530

    Add homepage title

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./home.styles.scss";
 
-const HomePage = () => <div className="home-page page"></div>;
+const HomePage = () => (
+    <div className="home-page page">
+        <div className="home-page-title">Welcome to the Practicals Hub!</div>
+    </div>
+);
 
 export default HomePage;
